4d260cd01ef879a3ef0a8caf2bc56aeff5714604
kimiimac <kimi0230@gmail.com>
2021-04-12 20:58:27 +0800

    0209.Minimum-Size-Subarray-Sum

--- 0209.Minimum-Size-Subarray-Sum/Minimum-Size-Subarray-Sum.go
+++ 0209.Minimum-Size-Subarray-Sum/Minimum-Size-Subarray-Sum.go
@@ -1,6 +1,9 @@
 package minimumsizesubarraysum
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
 // MinSubArrayLenBurst : 暴力解 時間複雜 O(n^2), 空間複雜 O(1)
 func MinSubArrayLenBurst(target int, nums []int) int {
@@ -51,9 +54,42 @@ func MinSubArrayLenSlidingWindow(target int, nums []int) int {
 	return minSize
 }
 
-// MinSubArrayLenBinarysearch : 前缀和 + 二分查找
+/*
+MinSubArrayLenBinarysearch : 前缀和 + 二分查找
+為了使用二分查找，需要額外創建一個數組 sums 用於存儲數組nums 的前綴和，其中 sums[i] 表示從 nums[0] 到 nums[i−1] 的元素和。
+得到前綴和之後，對於每個開始下標i，可通過二分查找得到大於或等於 i 的最小下標 bound，
+使得 sums[bound]-sums[i-1] >= s，
+並更新子數組的最小長度（此時子數組的長度是 bound -(i-1) )。
+C++ 的 lower_bound，Java 的 Arrays.binarySearch
+
+因為這道題保證了數組中每個元素都為正，所以前綴和一定是遞增的，這一點保證了二分的正確性。如果題目沒有說明數組中每個元素都為正，這裡就不能使用二分來查找這個位置了。
+*/
 func MinSubArrayLenBinarysearch(target int, nums []int) int {
+	lens := len(nums)
+	if lens <= 0 {
+		return 0
+	}
 	minSize := math.MaxInt32
+
+	sums := make([]int, lens+1)
+	// 為了方便計算 size = lens + 1
+	// sums[0] = 0, 前0個的元素和
+	// sums[1] = nums[0] , 前1個元素和為 nums[0]
+	for i := 1; i <= lens; i++ {
+		sums[i] = sums[i-1] + nums[i-1]
+	}
+
+	for i := 1; i <= lens; i++ {
+		tmpTarge := target + sums[i-1]
+		bound := sort.SearchInts(sums, tmpTarge)
+		// if bound < 0 {
+		// 	bound = -bound - 1
+		// }
+		if bound <= lens {
+			minSize = min(minSize, bound-(i-1))
+		}
+	}
+
 	if minSize == math.MaxInt32 {
 		minSize = 0
 	}
--- 0209.Minimum-Size-Subarray-Sum/Minimum-Size-Subarray-Sum_test.go
+++ 0209.Minimum-Size-Subarray-Sum/Minimum-Size-Subarray-Sum_test.go
@@ -41,3 +41,37 @@ func TestMinSubArrayLenSlidingWindow(t *testing.T) {
 		}
 	}
 }
+
+func TestMinSubArrayLenBinarysearch(t *testing.T) {
+	for _, tt := range tests {
+		if got := MinSubArrayLenBinarysearch(tt.arg1, tt.arg2); got != tt.want {
+			t.Errorf("got = %v \n want = %v \n", got, tt.want)
+		}
+	}
+}
+
+func BenchmarkMinSubArrayLenBurst(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		MinSubArrayLenBurst(tests[0].arg1, tests[0].arg2)
+	}
+}
+func BenchmarkMinSubArrayLenSlidingWindow(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		MinSubArrayLenSlidingWindow(tests[0].arg1, tests[0].arg2)
+	}
+}
+func BenchmarkMinSubArrayLenBinarysearch(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		MinSubArrayLenBinarysearch(tests[0].arg1, tests[0].arg2)
+	}
+}
+
+/*
+go test -benchmem -run=none LeetcodeGolang/0209.Minimum-Size-Subarray-Sum -bench=.
+BenchmarkMinSubArrayLenBurst-4                  27345652                41.67 ns/op            0 B/op          0 allocs/op
+BenchmarkMinSubArrayLenSlidingWindow-4          87522610                13.67 ns/op            0 B/op          0 allocs/op
+BenchmarkMinSubArrayLenBinarysearch-4            8496684               136.7 ns/op            64 B/op          1 allocs/op
+*/
