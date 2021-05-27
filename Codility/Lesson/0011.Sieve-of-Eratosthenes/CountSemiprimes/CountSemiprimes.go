22b567d67ed37f610bc0ee376a93426670d4543d
kimi <kimi0230@gmail.com>
2021-05-27 11:15:59 +0800

    countsemiprimes

--- Codility/Lesson/0011.Sieve-of-Eratosthenes/CountSemiprimes/CountSemiprimes.go
+++ Codility/Lesson/0011.Sieve-of-Eratosthenes/CountSemiprimes/CountSemiprimes.go
@@ -1,50 +1,6 @@
 package countsemiprimes
 
-import "fmt"
-
-// TODO:
 func Solution(N int, P []int, Q []int) []int {
-	prime := make([]int, N+1)
-	i := 2
-	for i*i <= N {
-		if prime[i] == 0 {
-			k := i * i
-			for k <= N {
-				if prime[k] == 0 {
-					prime[k] = i
-				}
-				k = k + i
-			}
-		}
-		i++
-	}
-	fmt.Println("prime: ", prime)
-
-	// 當前一共有幾個半質數
-	semiprime := make([]int, N+1)
-	for i := 1; i < len(prime); i++ {
-		p := prime[i]
-		if p == 0 {
-			semiprime[i] = semiprime[i-1]
-			continue
-		}
-		if prime[i/p] == 0 {
-			semiprime[i] = semiprime[i-1] + 1
-		} else {
-			semiprime[i] = semiprime[i-1]
-		}
-	}
-	fmt.Println("semiprime: ", semiprime)
-
-	result := make([]int, len(P))
-	for i, p := range P {
-		result[i] = semiprime[Q[i]] - semiprime[p-1]
-	}
-	return result
-}
-
-// TODO: Wrong answer. Need check again
-func Solution2(N int, P []int, Q []int) []int {
 	semiPrime := []int{}
 
 	// 半質數:兩個質數的乘積所得的自然數我們稱之為半質數.
@@ -71,6 +27,7 @@ func Solution2(N int, P []int, Q []int) []int {
 			if factorCount > 4 {
 				sign = 1
 				semiPrime = append(semiPrime, 0)
+				break
 			}
 		}
 		if sign != 1 {
@@ -81,13 +38,17 @@ func Solution2(N int, P []int, Q []int) []int {
 			}
 		}
 	}
-	fmt.Println("semiPrime", semiPrime)
+	// idx		 0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+	// semiPrime 0 0 0 4 0 6 0 0 9 10  0  0  0 14 15  0  0  0  0  0 21 22  0  0 25 26
+
+	// fmt.Println("semiPrime", semiPrime)
 
 	// 當前array和前面一共有幾個半質數
 	indexMap := make(map[int]int)
 	// 如果是半質數添加到 map
 	semiMap := make(map[int]int)
 	count := 0
+
 	for i := 0; i < len(semiPrime); i++ {
 		if semiPrime[i] != 0 {
 			count++
@@ -97,16 +58,59 @@ func Solution2(N int, P []int, Q []int) []int {
 			indexMap[i+1] = count
 		}
 	}
-	fmt.Println("indexMap : ", indexMap)
-	fmt.Println("semiMap : ", semiMap)
+	// 	indexMap :  map[1:0 2:0 3:0 4:1 5:1 6:2 7:2 8:2 9:3 10:4 11:4 12:4 13:4 14:5 15:6 16:6 17:6 18:6 19:6 20:6 21:7 22:8 23:8 24:8 25:9 26:10]
+	// semiMap :  map[4:0 6:0 9:0 10:0 14:0 15:0 21:0 22:0 25:0 26:0]
+	// fmt.Println("indexMap : ", indexMap)
+	// fmt.Println("semiMap : ", semiMap)
 
 	result := []int{}
 	for i := 0; i < len(P); i++ {
 		if _, ok := semiMap[P[i]]; ok {
-			result = append(result, indexMap[Q[i]]-indexMap[P[i-1]])
+			result = append(result, indexMap[Q[i]]-indexMap[P[i]]+1)
 		} else {
 			result = append(result, indexMap[Q[i]]-indexMap[P[i]])
 		}
+	}
+	return result
+}
+
+// TODO:
+func Solution2(N int, P []int, Q []int) []int {
+	prime := make([]int, N+1)
+	i := 2
+	for i*i <= N {
+		if prime[i] == 0 {
+			k := i * i
+			for k <= N {
+				if prime[k] == 0 {
+					prime[k] = i
+				}
+				k = k + i
+			}
+		}
+		i++
+	}
+	// fmt.Println("prime: ", prime)
+
+	// 當前一共有幾個半質數
+	semiprime := make([]int, N+1)
+	for i := 1; i < len(prime); i++ {
+		p := prime[i]
+		if p == 0 {
+			semiprime[i] = semiprime[i-1]
+			continue
+		}
+		if prime[i/p] == 0 {
+			semiprime[i] = semiprime[i-1] + 1
+		} else {
+			semiprime[i] = semiprime[i-1]
+		}
+	}
+	// fmt.Println("semiprime: ", semiprime)
+
+	result := make([]int, len(P))
+	for i, p := range P {
+		result[i] = semiprime[Q[i]] - semiprime[p-1]
 	}
 	return result
 }
