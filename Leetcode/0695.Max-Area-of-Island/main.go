008dfca550db618736ee06d601a059b0adf9c90c
kimi <kimi0230@gmail.com>
2023-07-06 18:14:57 +0800

    docs: tmp 695. Max Area of Island

--- Leetcode/0695.Max-Area-of-Island/main.go
+++ Leetcode/0695.Max-Area-of-Island/main.go
@@ -1,5 +1,26 @@
 package maxareaofisland
 
-func maxAreaOfIsland(grid [][]int) int {
+func MaxAreaOfIsland(grid [][]int) int {
+	result := 0
 
+	for i, row := range grid {
+		for j, col := range row {
+			if col == 1 {
+				result = max(result, areaOfIsland(grid, i, j))
+			}
+		}
+	}
+	return result
+}
+
+// DFS
+func areaOfIsland(grid [][]int, x, y int) int {
+	return 0
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
--- Leetcode/0695.Max-Area-of-Island/main_test.go
+++ Leetcode/0695.Max-Area-of-Island/main_test.go
@@ -3,32 +3,36 @@ package maxareaofisland
 import "testing"
 
 var tests = []struct {
-	arg1 string
+	arg1 [][]int
 	want int
 }{
 	{
-		"bbbab",
-		4,
+		[][]int{{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}},
+		6,
+	},
+	{
+		[][]int{{0, 0, 0, 0, 0, 0, 0, 0}},
+		0,
 	},
 }
 
-func TestLongestPalindromeSubseq(t *testing.T) {
+func TestMaxAreaOfIsland(t *testing.T) {
 	for _, tt := range tests {
 		// if got := ReverseList(tt.arg1); !reflect.DeepEqual(got, tt.want) {
-		if got := LongestPalindromeSubseq(tt.arg1); got != tt.want {
+		if got := MaxAreaOfIsland(tt.arg1); got != tt.want {
 			t.Errorf("got = %v, want = %v", got, tt.want)
 		}
 	}
 }
 
-func BenchmarkLongestPalindromeSubseq(b *testing.B) {
+func BenchmarkMaxAreaOfIsland(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		LongestPalindromeSubseq(tests[0].arg1)
+		MaxAreaOfIsland(tests[0].arg1)
 	}
 }
 
 /*
-go test -benchmem -run=none LeetcodeGolang/Leetcode/0354.Russian-Doll-Envelopes -bench=.
+go test -benchmem -run=none LeetcodeGolang/Leetcode/0695.Max-Area-of-Island -bench=.
 
 */
