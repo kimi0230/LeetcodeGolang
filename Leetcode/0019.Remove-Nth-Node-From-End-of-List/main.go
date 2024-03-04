b743314597a1f55387e4bbd106daf1cb7973ac44
kimiimac <kimi0230@gmail.com>
2024-03-04 23:20:07 +0800

    feat: 19.Renove Nth Node from end of list

--- Leetcode/0019.Remove-Nth-Node-From-End-of-List/main.go
+++ Leetcode/0019.Remove-Nth-Node-From-End-of-List/main.go
@@ -13,6 +13,10 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 產生 dummyHead,跟 preslow
+// 使用雙指針, 先讓 fast走 `k` 步, 然後 `fast slow 同速前進`
+// 這樣當fast走到nil時, slow所在位置就是在倒數第 k 的節點
+// 將 slow的前一步(preslow)的next 指向 slow.Next
 func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
 	dummyHead := &ListNode{Next: head}
 	preSlow, slow, fast := dummyHead, head, head
